64efe0447351870c98078120d3bed2198a7ca0bb
ganeshburri <ganeshburri1@gmail.com>
2024-03-07 17:37:29 +0530

    adding day31 challenge

---
+++ WEEK5/Day31/task3.py
@@ -0,0 +1,7 @@
+fav_movies = ['salaar', 'kgf', 'bahubali', 'kgf2']
+fav_movies.append('12th fail')
+release_dates = (2022, 2018, 2015, 2022, 2023)
+print(f"Movie: {fav_movies[2]} \nRelease Date: {release_dates[2]}")
+print('darling' in fav_movies)
+list = fav_movies + ['darling','f3']
+print(list)
