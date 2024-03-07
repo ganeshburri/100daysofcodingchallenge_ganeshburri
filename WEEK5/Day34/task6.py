570c045dceb39d7d3c252e504f91781fafd2cd9a
ganeshburri <ganeshburri1@gmail.com>
2024-03-07 19:05:23 +0530

    adding day34 challenge

---
+++ WEEK5/Day34/task6.py
@@ -0,0 +1,11 @@
+def calculate_area(length, width):
+    return length * width
+print(calculate_area(8, 5))
+person = {
+    "name": "John",
+    "age": 36,
+    "city": "Hyd"
+}
+def print_person_info(p):
+    print(f"Name: {p['name']}, Age: {p['age']}, City: {p['city']}")
+print(print_person_info(person))
