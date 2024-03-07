602227682dc41af1fd734f3d5e34082f1ddfd1d9
ganeshburri <ganeshburri1@gmail.com>
2024-03-07 17:40:38 +0530

    adding day31 challenge

--- WEEK5/Day31/task3.py
+++ WEEK5/Day31/task3.py
@@ -1,7 +1,7 @@
-fav_movies = ['salaar', 'kgf', 'bahubali', 'kgf2']
+fav_movies = ['salaar', 'kgf', 'bahubali', 'kgf2'] #List
 fav_movies.append('12th fail')
-release_dates = (2022, 2018, 2015, 2022, 2023)
+release_dates = (2022, 2018, 2015, 2022, 2023) #Tuple
 print(f"Movie: {fav_movies[2]} \nRelease Date: {release_dates[2]}")
 print('darling' in fav_movies)
-list = fav_movies + ['darling','f3']
+list = fav_movies + ['darling','f3'] #Concatenation
 print(list)
